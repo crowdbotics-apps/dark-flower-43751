4999bfa7cb2623c69dfb77e2c838592acb52f1b1
Crowdbotics <engineering@crowdbotics.com>
2023-10-30 11:19:05 +0000

    Update store.

--- store/darkflowerAPI/api.js
+++ store/darkflowerAPI/api.js
@@ -1,72 +1,52 @@
-import axios from "axios";
+import axios from "axios"
 const darkflowerAPI = axios.create({
   baseURL: "https://dark-flower-43751.botics.co",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json"
-  }
-});
-
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
+})
 function api_docs_schema_retrieve(payload) {
   return darkflowerAPI.get(`/api-docs/schema/`, {
-    params: {
-      lang: payload.lang
-    }
-  });
+    params: { lang: payload.lang }
+  })
 }
-
 function api_v1_login_create(payload) {
-  return darkflowerAPI.post(`/api/v1/login/`, payload);
+  return darkflowerAPI.post(`/api/v1/login/`, payload)
 }
-
 function api_v1_signup_create(payload) {
-  return darkflowerAPI.post(`/api/v1/signup/`, payload);
+  return darkflowerAPI.post(`/api/v1/signup/`, payload)
 }
-
 function rest_auth_login_create(payload) {
-  return darkflowerAPI.post(`/rest-auth/login/`, payload);
+  return darkflowerAPI.post(`/rest-auth/login/`, payload)
 }
-
 function rest_auth_logout_retrieve(payload) {
-  return darkflowerAPI.get(`/rest-auth/logout/`);
+  return darkflowerAPI.get(`/rest-auth/logout/`)
 }
-
 function rest_auth_logout_create(payload) {
-  return darkflowerAPI.post(`/rest-auth/logout/`);
+  return darkflowerAPI.post(`/rest-auth/logout/`)
 }
-
 function rest_auth_password_change_create(payload) {
-  return darkflowerAPI.post(`/rest-auth/password/change/`, payload);
+  return darkflowerAPI.post(`/rest-auth/password/change/`, payload)
 }
-
 function rest_auth_password_reset_create(payload) {
-  return darkflowerAPI.post(`/rest-auth/password/reset/`, payload);
+  return darkflowerAPI.post(`/rest-auth/password/reset/`, payload)
 }
-
 function rest_auth_password_reset_confirm_create(payload) {
-  return darkflowerAPI.post(`/rest-auth/password/reset/confirm/`, payload);
+  return darkflowerAPI.post(`/rest-auth/password/reset/confirm/`, payload)
 }
-
 function rest_auth_registration_create(payload) {
-  return darkflowerAPI.post(`/rest-auth/registration/`, payload);
+  return darkflowerAPI.post(`/rest-auth/registration/`, payload)
 }
-
 function rest_auth_registration_verify_email_create(payload) {
-  return darkflowerAPI.post(`/rest-auth/registration/verify-email/`, payload);
+  return darkflowerAPI.post(`/rest-auth/registration/verify-email/`, payload)
 }
-
 function rest_auth_user_retrieve(payload) {
-  return darkflowerAPI.get(`/rest-auth/user/`);
+  return darkflowerAPI.get(`/rest-auth/user/`)
 }
-
 function rest_auth_user_update(payload) {
-  return darkflowerAPI.put(`/rest-auth/user/`, payload);
+  return darkflowerAPI.put(`/rest-auth/user/`, payload)
 }
-
 function rest_auth_user_partial_update(payload) {
-  return darkflowerAPI.patch(`/rest-auth/user/`, payload);
+  return darkflowerAPI.patch(`/rest-auth/user/`, payload)
 }
-
 export const apiService = {
   api_docs_schema_retrieve,
   api_v1_login_create,
@@ -82,4 +62,4 @@ export const apiService = {
   rest_auth_user_retrieve,
   rest_auth_user_update,
   rest_auth_user_partial_update
-};
+}
